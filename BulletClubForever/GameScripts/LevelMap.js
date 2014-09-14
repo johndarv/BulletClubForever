641750af3719840a72002b4aa0123b7215dcc404
JohnDarv <JohnDarv@Hotmail.com>
2014-09-14 16:55:06 +0100

    Raw roguelike map now working.

--- BulletClubForever/GameScripts/LevelMap.js
+++ BulletClubForever/GameScripts/LevelMap.js
@@ -1,19 +1,70 @@
-﻿LevelMap = function (game) {
-    this.game = game;
+﻿LevelMap = function (x, y) {
     this.map = [];
-    this.x = 100;
-    this.y = 20;
+    this.x = x;
+    this.y = y;
 }
 
 LevelMap.prototype = {
     init: function () {
-        for (var i = 0; i < this.y; i++) {
+        for (var i = 0; i < this.x; i++) {
             // First, create the array within the array
             this.map[i] = [];
 
-            for (var j = 0; j < this.x; j++) {
+            for (var j = 0; j < this.y; j++) {
                 this.map[i][j] = ".";
             }
         }
+
+        this.initPlatforms();
+        this.overrideFirstPlatform();
+    },
+
+    initPlatforms: function () {
+        for (var i = 0; i < this.x; i++) {
+            this.makePieceOfMap(i);
+        }
+    },
+
+    overrideFirstPlatform: function () {
+        this.map[0][this.y - 1] = "G";
+    },
+
+    makeNewPieceOfMap: function () {
+        this.map[this.x] = [];
+        this.makePieceOfMap(this.x);
+        this.x++;
+    },
+
+    makePieceOfMap: function (xAxisPos) {
+        var hasGround = !createRandomBool(9);
+        var groundPiece = "."
+
+        // The ground tile
+        if (hasGround) {
+            groundPiece = "G";
+        }
+        
+        this.map[xAxisPos][this.y - 1] = groundPiece;
+
+        // Now cycle through all but the ground tile and randomly create platforms
+        for (var i = 0; i < this.y - 1; i++) {
+            var hasPlatform = createRandomBool(20);
+            var newPiece = ".";
+
+            if (hasPlatform) {
+                newPiece = "P";
+            }
+
+            this.map[xAxisPos][i] = newPiece;
+        }
     }
+}
+
+function createRandom(n) {
+    return Math.floor((Math.random() * n) + 1);
+}
+
+function createRandomBool(n) {
+    var r = createRandom(n);
+    return r === 1;
 }
--- BulletClubForever/Tests/LevelMapTests.js
+++ BulletClubForever/Tests/LevelMapTests.js
@@ -1,23 +1,27 @@
 ﻿QUnit.test("Test initial map is all set to .", function (assert) {
-    var levelMap = new LevelMap();
+    var x = 100;
+    var y = 20;
+    var levelMap = new LevelMap(x, y);
 
     assert.ok(levelMap.map !== undefined, "map property is initially not undefined.");
     assert.ok(levelMap.map.length === 0, "map property is initially empty.");
 
     levelMap.init();
 
-    assert.ok(levelMap.map.length === levelMap.y, "y axis is correct length.");
-    assert.ok(levelMap.map[0].length === levelMap.x, "x axis is correct length.");
+    assert.ok(levelMap.map.length === x, "x axis is correct length.");
+    assert.ok(levelMap.map[0].length === y, "y axis is correct length.");
+});
 
-    var passed = true;
 
-    for (var i = 0; i < levelMap.y; i++) {
-        for (var j = 0; j < levelMap.x; j++) {
-            if (levelMap.map[i][j] !== ".") {
-                passed = false;
-            }
-        }
-    }
 
-    assert.ok(passed, "map property is initially set to all dots.");
+QUnit.test("Test createRandom - number between 1 and 5 inclusive.", function (assert) {
+    var aRandomNumber = createRandom(5);
+    assert.ok(aRandomNumber !== NaN, "A number was created.");
+    assert.ok(aRandomNumber < 6, "The number was less than 6.");
+    assert.ok(aRandomNumber > 0, "The number was greater than 0.");
+});
+
+QUnit.test("Tests createRandomBool - creates a boolean.", function (assert) {
+    var aBool = createRandomBool(10);
+    assert.ok((aBool === true) || (aBool === false), "The boolean is either true or false.");
 });
